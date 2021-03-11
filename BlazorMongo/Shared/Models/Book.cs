63515bcf215ea293c96d7ab2715d76a6ffc7f9b0
Luis Harvey Triana Vega <harveytriana@hotmail.com>
2021-03-11 17:05:19 -0500

    Simplified model

--- BlazorMongo/Shared/Models/Book.cs
+++ BlazorMongo/Shared/Models/Book.cs
@@ -13,20 +13,10 @@ namespace BlazorMongo.Shared.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
-
-        [BsonElement("Name")]
         public string Name { get; set; }
-
-        [BsonElement("Author")]
         public string Author { get; set; }
-
-        [BsonElement("InStock")]
         public bool InStock { get; set; }
-
-        [BsonElement("Price")]
         public decimal Price { get; set; }
-
-        [BsonElement("CreationDate")]
         public DateTime CreationDate { get; set; } = DateTime.UtcNow;
 
         public override string ToString() => $"{Name}, {Author}";
